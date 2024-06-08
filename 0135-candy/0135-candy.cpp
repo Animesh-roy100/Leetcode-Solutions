2bfeec077bc077b698f3a12e2d098f5c0fd61a65
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-09 01:35:00 +0530

    Time: 19 ms (24.02%), Space: 19.4 MB (94.01%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,23 +2,46 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n, 1);
-        for (int i=1; i<n; i++) {
-            if(ratings[i]>ratings[i-1]) {
-                left[i] = left[i-1]+1;   
-            }
-        }
+//         vector<int> left(n, 1);
+//         for (int i=1; i<n; i++) {
+//             if(ratings[i]>ratings[i-1]) {
+//                 left[i] = left[i-1]+1;   
+//             }
+//         }
     
-        int curr=1, sum=left[n-1];
-        for (int i=n-2; i>=0; i--) {
-            if(ratings[i]>ratings[i+1]) {
-                curr++;
-            } else {
-                curr=1;
-            }
-            sum += max(left[i], curr);
-        }
+//         int curr=1, sum=left[n-1];
+//         for (int i=n-2; i>=0; i--) {
+//             if(ratings[i]>ratings[i+1]) {
+//                 curr++;
+//             } else {
+//                 curr=1;
+//             }
+//             sum += max(left[i], curr);
+//         }
         
+//         return sum;
+        
+        int sum=1, i=1;
+        while(i<n) {
+            if(ratings[i] == ratings[i-1]) {
+                sum += 1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n and ratings[i]>ratings[i-1]) {
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int down=1;
+            while(i<n and ratings[i]<ratings[i-1]) {
+                sum += down;
+                i++;
+                down++;
+            }
+            if (down>peak) sum += (down-peak);
+        }
         return sum;
     }
 };
