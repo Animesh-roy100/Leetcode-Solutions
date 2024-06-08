ce249855724e6772c2ab7583399619e83cdf9a6f
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-09 01:35:22 +0530

    Time: 11 ms (84.53%), Space: 19.6 MB (87.12%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,25 +2,6 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-//         vector<int> left(n, 1);
-//         for (int i=1; i<n; i++) {
-//             if(ratings[i]>ratings[i-1]) {
-//                 left[i] = left[i-1]+1;   
-//             }
-//         }
-    
-//         int curr=1, sum=left[n-1];
-//         for (int i=n-2; i>=0; i--) {
-//             if(ratings[i]>ratings[i+1]) {
-//                 curr++;
-//             } else {
-//                 curr=1;
-//             }
-//             sum += max(left[i], curr);
-//         }
-        
-//         return sum;
-        
         int sum=1, i=1;
         while(i<n) {
             if(ratings[i] == ratings[i-1]) {
