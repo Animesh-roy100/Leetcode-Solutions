46d2eab5d4f0593c8c21b0853c9604bf605d1329
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-09 00:44:01 +0530

    Time: 14 ms (63.61%), Space: 20.7 MB (10.81%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -5,18 +5,14 @@ public:
         vector<int> left(n, 1);
         for (int i=1; i<n; i++) {
             if(ratings[i]>ratings[i-1]) {
-                while(left[i] <= left[i-1]){
-                    left[i]++;   
-                }
+                left[i] = left[i-1]+1;   
             }
         }
         
         vector<int> right(n, 1);
         for (int i=n-2; i>=0; i--) {
             if(ratings[i]>ratings[i+1]) {
-                while(right[i] <= right[i+1]) {
-                    right[i]++;   
-                }
+                right[i] = right[i+1]+1;
             }
         }
         
