f6778d9a9a636f26e0ae686350c3fdca0d4a218a
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-12 13:55:03 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (34.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero=0, one=0, two=0;
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i]==0) zero++;
-            else if(nums[i]==1) one++;
-            else two++;
-        }
-        int i=0;
-        while(i<nums.size()) {
-            if(zero>0) {
-                while(zero--) {
-                    nums[i]=0;
-                    i++;
-                }
-            }
-            if(one>0) {
-                while(one--) {
-                    nums[i]=1;
-                    i++;
-                }
-            }
-            if(two>0) {
-                 while(two--) {
-                    nums[i]=2;
-                    i++;
-                }
-            }
+        // int zero=0, one=0, two=0;
+        // for(int i=0; i<nums.size(); i++) {
+        //     if(nums[i]==0) zero++;
+        //     else if(nums[i]==1) one++;
+        //     else two++;
+        // }
+        // int i=0;
+        // while(i<nums.size()) {
+        //     if(zero>0) {
+        //         while(zero--) {
+        //             nums[i]=0;
+        //             i++;
+        //         }
+        //     }
+        //     if(one>0) {
+        //         while(one--) {
+        //             nums[i]=1;
+        //             i++;
+        //         }
+        //     }
+        //     if(two>0) {
+        //          while(two--) {
+        //             nums[i]=2;
+        //             i++;
+        //         }
+        //     }
+        // }
+        int n = nums.size();
+        int l=0, r=n-1, mid=0;
+        while(mid<=r) {
+            if(nums[mid]==0) swap(nums[mid], nums[l++]);
+            else if (nums[mid]==2) swap(nums[mid--], nums[r--]);
+            mid++;
         }
     }
 };
