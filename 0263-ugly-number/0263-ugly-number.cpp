f7e95c3a09d0664c35e6f13c39e46ac94a040f26
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-15 18:35:53 +0530

    Time: 5 ms (24.30%), Space: 7.2 MB (24.37%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     bool isUgly(int n) {
         if (n==0) return false;
-        vector<int> prime({2, 3, 5});
-        for(int i=0; i<prime.size(); i++) {
-            while(n%prime[i]==0) {
-                n/=prime[i];
-            }
+        // vector<int> prime({2, 3, 5});
+        // for(int i=0; i<prime.size(); i++) {
+        //     while(n%prime[i]==0) {
+        //         n/=prime[i];
+        //     }
+        // }
+        while(n%2==0) {
+            n/=2;
+        }
+        while(n%3==0) {
+            n/=3;
+        }
+        while(n%5==0) {
+            n/=5;
         }
         if(n==1) return true;
         return false;
