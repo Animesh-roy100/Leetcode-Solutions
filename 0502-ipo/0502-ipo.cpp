61d67d35cc94293f305c03e3d7d923fb3ba6bc0e
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-16 02:29:09 +0530

    Time: 157 ms (33.86%), Space: 85.5 MB (62.81%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> minHeap;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
         
-        int n=profits.size();
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<profits.size(); i++) {
             minHeap.push({capital[i], profits[i]});
         }
         
-        // cout<<minHeap.top().second;
-        
         priority_queue<int> maxHeap;
+        
         while(k--) {
             while(!minHeap.empty() and minHeap.top().first <= w) {
                 maxHeap.push(minHeap.top().second);
                 minHeap.pop();
             }
-            if(maxHeap.empty()) {
-                return w;
-            }
+            
+            if(maxHeap.empty()) return w;
+            
             w += maxHeap.top();
             maxHeap.pop();
         }
+        
         return w;
     }
 };
