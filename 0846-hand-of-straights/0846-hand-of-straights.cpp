96449abc2bac254847d0f82e44daf4d4ad3e7901
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-18 19:18:10 +0530

    Time: 77 ms (10.44%), Space: 30.6 MB (61.14%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        if (hand.size() % groupSize != 0) return false;
-       map<int, int> count;
-
-    for (int card : hand)
-      ++count[card];
-
-    for (auto& it : count) {
-      int value = count[it.first];
-      if (value > 0)
-        for (int i = it.first; i < it.first + groupSize; ++i) {
-          count[i] -= value;
-          if (count[i] < 0)
-            return false;
+        if(hand.size() % groupSize != 0) return false;
+        map<int, int> umap;
+        for(int i=0; i<hand.size(); i++) {
+            umap[hand[i]]++;
         }
-    }
-
-    return true;
+        
+        for(auto &it:umap) {
+            int value = it.second;
+            if(value>0) {
+                for(int i=it.first; i<it.first+groupSize; i++) {
+                    umap[i] -= value;
+                    if (umap[i] < 0) return false;
+                }
+            }
+        }
+        return true;
     }
 };
