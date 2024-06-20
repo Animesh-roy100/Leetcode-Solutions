f5427a6913913f881868151dacd31df774e218b0
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-21 03:16:54 +0530

    Time: 11 ms (59.83%), Space: 13.8 MB (56.32%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     void bfs(vector<vector<char>>& board, int i, int j) {
         int m=board.size(), n=board[0].size();
-        if(i<0 or i>=m or j<0 or j>=n or board[i][j]!='O') return;
+//         if(i<0 or i>=m or j<0 or j>=n or board[i][j]!='O') return;
         
-        board[i][j] = 'A';
+//         board[i][j] = 'A';
         
-        bfs(board, i+1, j);
-        bfs(board, i-1, j);
-        bfs(board, i, j+1);
-        bfs(board, i, j-1);
+//         bfs(board, i+1, j);
+//         bfs(board, i-1, j);
+//         bfs(board, i, j+1);
+//         bfs(board, i, j-1);
         
-//         queue<pair<int, int>> q;
-//         q.push({i, j});
+        queue<pair<int, int>> q;
+        q.push({i, j});
         
-//         vector<pair<int, int>> dir{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        vector<pair<int, int>> dir{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
-//         while(!q.empty()) {
-//             int x = q.front().first;
-//             int y = q.front().second;
-//             q.pop();
+        while(!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
             
-//             if(x<0 or x>=m or y<0 or y>=n or board[x][y]!='O') return;
+            if(x<0 or x>=m or y<0 or y>=n or board[x][y]!='O') continue;
             
-//             board[x][y] = 'A';
+            board[x][y] = 'A';
             
-//             for(auto &it: dir) {
-//                 q.push({x+it.first, y+it.second});
-//             }
-//         }
+            for(auto &it: dir) {
+                q.push({x+it.first, y+it.second});
+            }
+        }
     }
     
     void solve(vector<vector<char>>& board) {
