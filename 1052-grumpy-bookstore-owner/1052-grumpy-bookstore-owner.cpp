8113351ccdea603347e0916882bfe3eb2fb6f638
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-21 19:29:54 +0530

    Time: 31 ms (21.86%), Space: 34.4 MB (47.97%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n=grumpy.size();
-        int ans=0, sum=0;
+        int n=customers.size();
+        int satisfied=0, ans=0;
         
-        for(int i=0; i<n; i++) {
-            if(grumpy[i]==0) sum += customers[i];
-        }
+        for(int i=0; i<customers.size(); i++) {
+            if(grumpy[i]==0) satisfied+=customers[i];
+        } 
         
         int l=0, r=minutes-1;
         for(int i=l; i<=r; i++) {
-            if(grumpy[i]==1) sum += customers[i];
-
+            if(grumpy[i]==1) {
+                satisfied+=customers[i];
+            }
         }
-        ans = max(ans, sum);
+        ans = max(ans, satisfied);
         
         while(r<n-1) {
-            if(grumpy[l] == 1) sum -= customers[l];
+            if(grumpy[l] == 1) satisfied-=customers[l];
             l++;
             r++;
-            if(grumpy[r] == 1) sum += customers[r];
-            ans = max(ans, sum);
+            if(grumpy[r] == 1) satisfied+=customers[r];
+            
+            ans = max(ans, satisfied);
         }
         
         return ans;
