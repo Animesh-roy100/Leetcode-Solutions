cb026e2728a0b9bb13bb014970eaffaba2fd3b60
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-21 15:43:31 +0530

    Time: 23 ms (66.42%), Space: 34.4 MB (47.97%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -7,23 +7,19 @@ public:
         for(int i=0; i<n; i++) {
             if(grumpy[i]==0) sum += customers[i];
         }
+        
         int l=0, r=minutes-1;
-        
         for(int i=l; i<=r; i++) {
-            if(grumpy[i]==1) {
-                sum += customers[i];
-            }
+            if(grumpy[i]==1) sum += customers[i];
+
         }
-        
         ans = max(ans, sum);
         
         while(r<n-1) {
             if(grumpy[l] == 1) sum -= customers[l];
-            l++; 
+            l++;
             r++;
-            
             if(grumpy[r] == 1) sum += customers[r];
-            
             ans = max(ans, sum);
         }
         
