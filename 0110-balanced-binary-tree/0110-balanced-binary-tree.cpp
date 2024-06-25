be9afb23258e054ee1c2203f49dd960a4d223548
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-25 19:23:06 +0530

    Time: 8 ms (71.77%), Space: 21.6 MB (41.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -15,21 +15,16 @@ public:
         if(root==NULL) return 0;
         
         int lh = check(root->left);
+        if(lh==-1) return -1;
         int rh = check(root->right);
-        
-        if(lh==-1 or rh==-1) return -1;
+        if(rh==-1) return -1;
+
         if(abs(lh-rh) > 1) return -1;
         
         return 1 + max(lh, rh);
     }
     
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr) return true;
-        
-        int height = check(root);
-        
-        if(height == -1) return false;
-        
-        return true;
+        return check(root)==-1 ? false : true;
     }
 };
