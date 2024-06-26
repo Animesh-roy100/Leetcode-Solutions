3431e5ba6d63f45ade7410657959321f52cc81dd
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-26 20:13:52 +0530

    Time: 19 ms (12.67%), Space: 24 MB (5.12%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -14,7 +14,16 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         vector<int> nums;
         inorder(root, nums);
-        return nums[k-1];
+        priority_queue<int> pq;
+        
+        for(int i=0; i<nums.size(); i++) {
+            pq.push(nums[i]);
+            if(pq.size()>k) {
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
     }
     
     void inorder(TreeNode* root, vector<int> &nums) {
