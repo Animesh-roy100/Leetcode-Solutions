f8fc54a529f9c77c5651f6b4a795ee28d3f938e8
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-27 01:16:36 +0530

    Time: 13 ms (62.15%), Space: 14.9 MB (88.47%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -23,18 +23,32 @@ public:
     vector<int> preorder(Node* root) {
         vector<int> ans;
         if(root==NULL) return ans;
-        ans.push_back(root->val);
-        preOrder(root, ans);
+        // ans.push_back(root->val);
+        // preOrder(root, ans);
+        
+        stack<Node*> stk;
+        stk.push(root);
+        
+        while(!stk.empty()) {
+            Node* curr = stk.top();
+            stk.pop();
+            
+            ans.push_back(curr->val);
+            
+            for(int i=curr->children.size()-1; i>=0; i--) {
+                stk.push(curr->children[i]);
+            }
+        }
         
         return ans;
     }
     
-    void preOrder(Node* root, vector<int> &ans) {
-        if(root==NULL) return;
+//     void preOrder(Node* root, vector<int> &ans) {
+//         if(root==NULL) return;
         
-        for(auto it: root->children) {
-            ans.push_back(it->val);
-            preOrder(it, ans);
-        }
-    }
+//         for(auto it: root->children) {
+//             ans.push_back(it->val);
+//             preOrder(it, ans);
+//         }
+//     }
 };
