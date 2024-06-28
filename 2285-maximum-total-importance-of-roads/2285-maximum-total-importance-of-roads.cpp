af0d33c8566a8bf7ec1c4cf9f30cdcca2f8223af
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-06-28 16:16:29 +0530

    Time: 248 ms (85.00%), Space: 116.3 MB (95.17%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> v(n);
+        
+        for(int i=0; i<roads.size(); i++) {
+            v[roads[i][0]]++;
+            v[roads[i][1]]++;
+        }
+        
+        sort(v.begin(), v.end());
+        
+        long long ans=0;
+        
+        for(int i=0; i<n; i++) {
+            ans += 1LL*v[i]*(i+1);
+        }
+        
+        return ans;
+    }
+};
