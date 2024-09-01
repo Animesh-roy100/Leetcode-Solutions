de0805ff9a36eeae46e1d043747b1cb0a6508586
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-09-01 15:11:29 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (9.94%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,11 +3,7 @@ public:
     int countSeniors(vector<string>& details) {
         int count=0;
         for(auto s: details) {
-            string str = s.substr(11, 2);
-            cout<<str<<" ";
-            if(stoi(str) > 60) {
-                count++;
-            }
+            if(stoi(s.substr(11, 2)) > 60) count++;
         }
         return count;
     }
