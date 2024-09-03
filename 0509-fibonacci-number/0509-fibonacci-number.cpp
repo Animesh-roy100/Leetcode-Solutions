a69a01dc73de49261b0156c06edc0a2bd7f49197
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-09-03 16:26:50 +0530

    Time: 3 ms (66.47%), Space: 7.6 MB (11.58%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        vector<int> fibo(n+1);
+        fibo[0] = 0;
+        fibo[1] = 1; 
+        for(int i=2; i<n+1; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+        return fibo[n];
+    }
+};
