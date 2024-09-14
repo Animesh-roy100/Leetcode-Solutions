aa6a90898dd714438f61c4c3089c8dcb513c0b46
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-09-14 20:03:09 +0530

    Time: 94 ms (85.75%), Space: 85 MB (34.21%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,15 +3,13 @@ public:
     int longestSubarray(vector<int>& nums) {
         int maxEle=0, count=0, ans=0;
         for(int i=0; i<nums.size(); i++) {
-            if(maxEle == nums[i]) {
-                count++;
-            } else if (maxEle < nums[i]) {
+            if(maxEle == nums[i]) count++;
+            else if (maxEle < nums[i]) {
                 maxEle = nums[i];
                 count=1;
                 ans=1;
-            } else {
-                count=0;
-            }
+            } else count=0;
+            
             ans = max(count, ans);
         }
         
