93fbbfd0d2184e0bbe77781cc75b3ada70b7f82a
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-11-05 20:08:04 +0530

    Time: 210 ms (24.13%), Space: 12.3 MB (67.23%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int i, int j, vector<vector<int>> &dp) {
+    int solve(vector<int> &balloons, int i, int j, vector<vector<int>> &dp) {
         if(i>j) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
         int maxCoins = 0;
+        
         for(int k=i; k<=j; k++) {
-            int coins = solve(nums, i, k-1, dp) + solve(nums, k+1, j, dp) + nums[i-1]*nums[k]*nums[j+1];
+            int coins = solve(balloons, i, k-1, dp) + solve(balloons, k+1, j, dp) + balloons[i-1]*balloons[k]*balloons[j+1];
             
             maxCoins = max(maxCoins, coins);
         }
-        
+    
         return dp[i][j] = maxCoins;
     }
     
     int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        nums.push_back(1);
-        nums.insert(nums.begin(), 1);
+        int n=nums.size();
+        vector<int> balloons(n+2, 0);
+        balloons[0] = 1;
+        for(int i=1; i<=n; i++) {
+            balloons[i] = nums[i-1];
+        }
+        balloons[n+1] = 1;
         
         vector<vector<int>> dp(n+2, vector<int> (n+2, -1));
-        return solve(nums, 1, n, dp);
+        
+        return solve(balloons, 1, n, dp);
     }
 };
