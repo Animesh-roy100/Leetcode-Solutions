640d1fa9fef52d57dae869060326bb80c55c5121
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-11-07 16:11:57 +0530

    Time: 28 ms (33.13%), Space: 36.2 MB (61.39%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size();
-        vector<vector<int>> ans(m, vector<int> (n, INT_MAX));
-        vector<vector<bool>> vis(m, vector<bool> (n, false));
-        vector<pair<int, int>> dir{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        int m = mat.size(), n = mat[0].size();
         
+        vector<pair<int, int>> dir{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        vector<vector<int>> ans(m, vector<int> (n, INT_MAX));
+        vector<vector<bool>> vis(m, vector<bool> (n , false));
         queue<pair<int, int>> q;
+        
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(mat[i][j] == 0)  {
+                if(mat[i][j] == 0) {
                     q.push({i, j});
                     ans[i][j] = 0;
                     vis[i][j] = true;
@@ -21,17 +22,16 @@ public:
             int x = q.front().first;
             int y = q.front().second;
             q.pop();
-
+            
             for(auto &it: dir) {
-                int newX = x + it.first;
-                int newY = y + it.second;
-
-                if(newX<0 or newX>=m or newY<0 or newY>=n or vis[newX][newY]) continue;
+                int X = x + it.first;
+                int Y = y + it.second;
                 
-                ans[newX][newY] = min(ans[newX][newY], 1+ans[x][y]); 
-                vis[newX][newY] = true;
-
-                q.push({newX, newY});
+                if(X<0 or X>=m or Y<0 or Y>=n or vis[X][Y]) continue;
+                
+                q.push({X, Y});
+                vis[X][Y] = true;
+                ans[X][Y] = min(ans[X][Y], 1 + ans[x][y]);
             }
         }
         
