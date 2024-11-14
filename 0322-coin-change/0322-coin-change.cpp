a1434cf04e3f43129abe8f72658c209340557a22
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-11-14 19:19:34 +0530

    Time: 66 ms (14.29%), Space: 52.5 MB (8.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,41 +1,21 @@
 class Solution {
 public:
-//     int solve(vector<int> &coins, int amount, vector<int> &dp) {
-//         if(amount == 0) return 0;
-//         if(amount < 0) return INT_MAX;
-//         if(dp[amount] != -1) return dp[amount];
+    int solve(vector<int> &coins, int i, int amt, vector<vector<int>> &dp) {
+        if(amt == 0) return 0;
+        if(i==0) return INT_MAX-1;
+        if(dp[i][amt] != -1) return dp[i][amt];
         
-//         int minCoins = INT_MAX;
-//         for(int i=0; i<coins.size(); i++) {
-//             int res = solve(coins, amount-coins[i], dp);
-//             if(res != INT_MAX) {
-//                 minCoins = min(minCoins, res+1);
-//             }
-//         }
-        
-//         dp[amount] = minCoins;
-        
-//         return minCoins;
-//     }
+        if(coins[i-1] <= amt) {
+            return dp[i][amt] = min(1 + solve(coins, i, amt - coins[i-1], dp), solve(coins, i-1, amt, dp));
+        } else {
+            return dp[i][amt] = solve(coins, i-1, amt, dp);
+        }
+    }
     
     int coinChange(vector<int>& coins, int amount) {
-        // int n= coins.size();
-        // vector<int> dp(amount+1, -1);
-        // int res = solve(coins, amount, dp);
-        // if(res == INT_MAX) return -1;
-        // return res;
-        
-        vector<int> dp(amount+1, INT_MAX);
-        dp[0]=0;
-        
-        for(int i=1; i<=amount; i++) {
-            for(int coin: coins) {
-                if(i-coin >= 0 and dp[i-coin] != INT_MAX) {
-                    dp[i] = min(dp[i], dp[i-coin]+1);
-                }
-            }
-        }
-        
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        int n=coins.size();
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
+        int res = solve(coins, n, amount, dp);
+        return res == INT_MAX-1 ? -1 : res;
     }
 };
