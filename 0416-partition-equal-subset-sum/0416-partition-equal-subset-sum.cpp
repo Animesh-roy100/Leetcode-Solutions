cb07f0b9fb845fa4c4ab12af02b121e6f9515c85
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-11-15 15:00:13 +0530

    Time: 294 ms (25.08%), Space: 16.5 MB (61.19%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int sum) {
+    bool solve(vector<int> &nums, int sum) {
         int n=nums.size();
         vector<vector<bool>> dp(n+1, vector<bool> (sum+1, false));
         dp[0][0] = true;
         for(int i=1; i<=n; i++) {
             for(int j=0; j<=sum; j++) {
-                if(j==0) dp[i][j] = true;
-                else {
+                // if(j==0) dp[i][j] = true;
+                // else {
                     if(nums[i-1] <= j) {
-                        dp[i][j] = dp[i-1][j - nums[i-1]] || dp[i-1][j];
+                        dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
                     } else {
                         dp[i][j] = dp[i-1][j];
                     }
-                }
+                // }
             }
         }
         
