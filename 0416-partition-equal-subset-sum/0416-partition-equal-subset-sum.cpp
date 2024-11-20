8b1994af0b94b3dde7cc31bfadece633161b685d
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-11-20 12:35:10 +0530

    Time: 47 ms (93.60%), Space: 99.7 MB (22.96%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    bool solve(vector<int> &nums, int sum) {
-        int n=nums.size();
-        vector<vector<bool>> dp(n+1, vector<bool> (sum+1, false));
-        dp[0][0] = true;
-        for(int i=1; i<=n; i++) {
-            for(int j=0; j<=sum; j++) {
-                // if(j==0) dp[i][j] = true;
-                // else {
-                    if(nums[i-1] <= j) {
-                        dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                    } else {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                // }
-            }
-        }
+    bool solve(vector<int> &nums, int n, int sum, vector<vector<int>> &dp) {
+        if(sum==0) return true;
+        if(n==0) return false;
         
-        return dp[n][sum];
+        if(dp[n][sum] != -1) return dp[n][sum];
+        if(nums[n-1] <= sum) {
+            return dp[n][sum] = solve(nums, n-1, sum-nums[n-1], dp) || solve(nums, n-1, sum, dp);
+        } else {
+            return dp[n][sum] = solve(nums, n-1, sum, dp);
+        }
     }
     
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(int num: nums) sum += num;
-        if(sum%2 != 0) return false;
+        for(int num: nums) sum+=num;
+        int n=nums.size();
+        if(sum%2 == 1) return false;
+        sum = sum/2;
         
-        return solve(nums, sum/2);
+        vector<vector<int>> dp(n+1, vector<int> (sum+1, -1));
+        return solve(nums, nums.size(), sum, dp);
     }
 };
