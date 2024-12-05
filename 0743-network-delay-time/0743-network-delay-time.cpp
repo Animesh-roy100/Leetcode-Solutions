f20ede223dc31c940f1315d0be546da64aaf3098
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-05 13:50:14 +0530

    Time: 85 ms (50.93%), Space: 47.9 MB (15.59%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -8,7 +8,6 @@ public:
         for(auto it: times) {
             adj[it[0]].push_back({it[1], it[2]});
         }
-        dist[0] = -1;
         dist[k] = 0;
         pq.push({0, k});
         
@@ -20,7 +19,7 @@ public:
             for(auto it: adj[node]) {
                 int currNode = it.first;
                 int currDist = it.second;
-                cout<<"curr details: "<<currNode<<" "<<currDist<<endl;
+                // cout<<"curr details: "<<currNode<<" "<<currDist<<endl;
                 if(dis + currDist < dist[currNode]) {
                     dist[currNode] = dis + currDist;
                     pq.push({dist[currNode], currNode});
@@ -28,7 +27,7 @@ public:
             }
         }
         
-        int ans = *max_element(dist.begin(), dist.end());
+        int ans = *max_element(dist.begin()+1, dist.end());
         return ans == INT_MAX ? -1 : ans;
     }
 };
