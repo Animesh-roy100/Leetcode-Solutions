77f83b36eda0d8fc721ac18004782faf976f1ae9
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-06 18:45:39 +0530

    Time: 24 ms (66.71%), Space: 21.2 MB (37.73%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -10,18 +10,18 @@ public:
 
 class Solution {
 public:
-    int solve(unordered_map<int, Employee*> &um, int &id) {
-        if(um.find(id) == um.end()) return 0;
+//     int solve(unordered_map<int, Employee*> &um, int &id) {
+//         if(um.find(id) == um.end()) return 0;
         
-        Employee* employee = um[id];
-        int totalImp = employee->importance;
-        // cout<<totalImp<<endl;
-        for(int index: employee->subordinates) {
-            totalImp += solve(um, index);
-        }
+//         Employee* employee = um[id];
+//         int totalImp = employee->importance;
+//         // cout<<totalImp<<endl;
+//         for(int index: employee->subordinates) {
+//             totalImp += solve(um, index);
+//         }
         
-        return totalImp;
-    }
+//         return totalImp;
+//     }
     
     int getImportance(vector<Employee*> employees, int id) {
         unordered_map<int, Employee*> um;
@@ -30,6 +30,20 @@ public:
             um[employee->id] = employee;
         }
         
-        return solve(um, id);
+        // return solve(um, id);
+        int result=0;
+        stack<Employee*> stk;
+        stk.push(um[id]);
+        
+        while(!stk.empty()) {
+            Employee* employee = stk.top();
+            stk.pop();
+            result += employee->importance;
+            for(int ind: employee->subordinates) {
+                stk.push(um[ind]);
+            }
+        }
+        
+        return result;
     }
 };
