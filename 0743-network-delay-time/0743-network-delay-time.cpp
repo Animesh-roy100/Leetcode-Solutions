54622c45e7974ed795fcd1ae05900a7ca1431854
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-06 15:27:10 +0530

    Time: 1027 ms (5.01%), Space: 279.6 MB (5.03%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> dist(n+1, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<vector<pair<int, int>>> adj(n+1);
-        
-        for(auto it: times) {
-            adj[it[0]].push_back({it[1], it[2]});
-        }
         dist[k] = 0;
-        pq.push({0, k});
-        
-        while(!pq.empty()) {
-            int node = pq.top().second;
-            int dis = pq.top().first;
-            pq.pop();
-            
-            for(auto it: adj[node]) {
-                int currNode = it.first;
-                int currDist = it.second;
-                // cout<<"curr details: "<<currNode<<" "<<currDist<<endl;
-                if(dis + currDist < dist[currNode]) {
-                    dist[currNode] = dis + currDist;
-                    pq.push({dist[currNode], currNode});
+        for(int i=0; i<n; i++) {
+            for(auto it: times) {
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                
+                if(dist[u] != INT_MAX and dist[u] + wt < dist[v]) {
+                    dist[v] = dist[u] + wt;
                 }
             }
         }
         
-        int ans = *max_element(dist.begin()+1, dist.end());
-        return ans == INT_MAX ? -1 : ans;
+        int maxWait=0;
+        for(int i=1; i<=n; i++) {
+            maxWait = max(maxWait, dist[i]);
+        }
+        
+        return maxWait == INT_MAX ? -1 : maxWait;
     }
 };
