ddaa6c69ea9daf0771671f380347708d3615e7f4
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-08 11:16:23 +0530

    Time: 0 ms (100.00%), Space: 13 MB (16.47%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>> &dungeon, vector<vector<int>> &dp) {
-        int m=dungeon.size(), n = dungeon[0].size();
-        if(i == m or j == n) return INT_MAX;
-        if(i == m-1 and j == n-1) return max(1, 1 - dungeon[i][j]);
+    // int solve(int i, int j, vector<vector<int>> &dungeon, vector<vector<int>> &dp) {
+//         int m=dungeon.size(), n = dungeon[0].size();
+//         if(i == m or j == n) return INT_MAX;
+//         if(i == m-1 and j == n-1) return max(1, 1 - dungeon[i][j]);
         
-        if(dp[i][j] != -1) return dp[i][j];
+//         if(dp[i][j] != -1) return dp[i][j];
         
-        int right = solve(i, j+1, dungeon, dp);
-        int down = solve(i+1, j, dungeon, dp);
+//         int right = solve(i, j+1, dungeon, dp);
+//         int down = solve(i+1, j, dungeon, dp);
         
-        return dp[i][j] = max(1, min(right, down) - dungeon[i][j]);
-    }
+//         return dp[i][j] = max(1, min(right, down) - dungeon[i][j]);
+//     }
     
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int m=dungeon.size(), n = dungeon[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return solve(0, 0, dungeon, dp);
+        // vector<vector<int>> dp(m, vector<int> (n, -1));
+        // return solve(0, 0, dungeon, dp);
+        
+        vector<vector<int>> dp(m+1, vector<int> (n+1, INT_MAX));
+        dp[m][n-1] = dp[m-1][n] = 1;
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                dp[i][j] = max(1, min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
