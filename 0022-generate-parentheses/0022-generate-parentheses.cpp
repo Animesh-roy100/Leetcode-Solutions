d4263447d20d1acb5d01fe6d3be30223184f05e9
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-10 19:34:19 +0530

    Time: 3 ms (69.38%), Space: 13.5 MB (73.93%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<string> ans;
-    void solve(string &s, int open, int close) {
+    void solve(string &s, int count, int open, int close) {
+        if(count<0) return;
         if(open==0 and close==0) {
             ans.push_back(s);
             return;
@@ -9,13 +10,13 @@ public:
         
         if(open > 0) {
             s.push_back('(');
-            solve(s, open-1, close);
+            solve(s, count+1, open-1, close);
             s.pop_back();
         } 
         
-        if(close > open){
+        if(close > 0){
             s.push_back(')');
-            solve(s, open, close-1);
+            solve(s, count-1, open, close-1);
             s.pop_back();
         }
         
@@ -23,7 +24,7 @@ public:
     
     vector<string> generateParenthesis(int n) {
         string s="";
-        solve(s, n, n);
+        solve(s, 0, n, n);
         return ans;
     }
 };
