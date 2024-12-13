cbf8be350b66b0240524c54f409ce9f0bda33cc1
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-13 15:40:30 +0530

    Time: 22 ms (83.33%), Space: 107.4 MB (25.71%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n=temp.size();
         vector<int> ans(n, 0);
-        stack<pair<int, int>> stk;
+        stack<int> stk;
         
         for(int i=0; i<n; i++) {
-            while(!stk.empty() and temp[i] > stk.top().second) {
-                int idx = stk.top().first;
-                int tmp = stk.top().second;
+            while(!stk.empty() and temp[i] > temp[stk.top()]) {
+                int idx = stk.top();
                 stk.pop();
                 ans[idx] = i-idx;
             }
-            stk.push({i, temp[i]});
+            stk.push(i);
         }
         
         return ans;
