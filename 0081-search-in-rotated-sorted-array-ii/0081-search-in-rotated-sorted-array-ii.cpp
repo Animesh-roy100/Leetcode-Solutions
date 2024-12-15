52aa9ed0e9bfb6fb90c6dff2ba0c13f701acfe59
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-15 11:12:19 +0530

    Time: 0 ms (100.00%), Space: 18 MB (15.39%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -3,33 +3,33 @@ public:
     bool search(vector<int>& nums, int target) {
         int n=nums.size();
         int start=0, end=n-1;
-        
+
         while(start<=end) {
             int mid = start + (end-start)/2;
-            
+
             if(nums[mid] == target) return true;
             
-            if(nums[start] == nums[mid] and nums[mid] == nums[end]) {
+            if(nums[mid] == nums[start] and nums[mid] == nums[end]) {
                 start++;
                 end--;
                 continue;
             }
             
             if(nums[mid] >= nums[start]) {
-                if(nums[start] <= target and nums[mid] >= target) {
+                if(nums[mid] >= target and nums[start] <= target) {
                     end = mid-1;
                 } else {
-                    start = mid+1;
+                    start = mid+1; 
                 }
-            } else if(nums[mid] <= nums[end]) {
-                if(nums[mid] <= target and target <= nums[end]) {
+            } else {
+                if(nums[mid] <= target and nums[end] >= target) {
                     start = mid+1;
                 } else {
-                    end = mid-1;
+                    end = mid-1; 
                 }
             }
         }
-        
+
         return false;
     }
 };
