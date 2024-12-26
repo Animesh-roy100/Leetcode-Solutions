2550283ad3e4fcdee25df4b1a19aa7a535947e8e
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2024-12-27 05:20:30 +0530

    Time: 0 ms (100.00%), Space: 17.3 MB (50.46%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int solve(int i, vector<int> &cost, vector<int> &dp) {
-        if(i<=1) return dp[i] = cost[i];
-        if(dp[i] != -1) return dp[i];
+//     int solve(int i, vector<int> &cost, vector<int> &dp) {
+//         if(i<=1) return dp[i] = cost[i];
+//         if(dp[i] != -1) return dp[i];
         
-        return dp[i] = cost[i] + min(solve(i-1, cost, dp), solve(i-2, cost, dp));
-    }
+//         return dp[i] = cost[i] + min(solve(i-1, cost, dp), solve(i-2, cost, dp));
+//     }
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n, -1);
-        return min(solve(n-1, cost, dp), solve(n-2, cost, dp));
+        // vector<int> dp(n, -1);
+        // return min(solve(n-1, cost, dp), solve(n-2, cost, dp));
+        
+        int prev1 = cost[1];
+        int prev2 = cost[0];
+        
+        for(int i=2; i<n; i++) {
+            int current = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return min(prev1, prev2);
     }
 };
