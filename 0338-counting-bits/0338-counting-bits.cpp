0a1f0e7dad53ba2f2e76b012736e2186e1abd88c
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-01-12 02:22:22 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.65%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        ans.push_back(0);
+        vector<int> ans(n+1, 0);
         for(int i=1; i<=n; i++) {
-            int count=0;
-            int num=i;
-            while(num>0) {
-                count += (num&1);
-                num = num>>1;
-            }
-            ans.push_back(count);
+            // int count=0;
+            // int num=i;
+            // while(num>0) {
+            //     count += (num&1);
+            //     num = num>>1;
+            // }
+            // ans[i]=count;
+            ans[i] = ans[i>>1] + (i&1);
         }
         return ans;
     }
