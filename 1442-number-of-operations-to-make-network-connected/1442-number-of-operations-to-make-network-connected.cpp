1ed44142b68debdc57b0bff0df73ff77b963f08b
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-01-14 20:46:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (90.6%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -24,11 +24,14 @@ private:
     }
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) return -1; // Not enough edges to connect all nodes
+
         parent.resize(n);
         rank.resize(n, 0);
         for(int i=0; i<n; i++) parent[i]=i;
         int extraEdges=0;
 
+        // Union-Find for all connections
         for(int i=0; i<connections.size(); i++) {
             int u = connections[i][0];
             int v = connections[i][1];
@@ -41,17 +44,25 @@ public:
                 extraEdges++;
             }
         }
-        unordered_set<int> st;
-        for(int i=0; i<n; i++) {
-            int parent = findParent(i);
-            if(st.find(parent) == st.end()) st.insert(parent);
+
+        // Count the number of connected components
+        // unordered_set<int> st;
+        // for(int i=0; i<n; i++) {
+        //     int parent = findParent(i);
+        //     if(st.find(parent) == st.end()) st.insert(parent);
+        // }
+        // int connComponents = st.size();
+
+        int connectedComponents = 0;
+        for (int i = 0; i < n; i++) {
+            if (findParent(i) == i) // Each unique parent represents one component
+                connectedComponents++;
         }
 
-        int connComponents = st.size();
-        cout<<"connected components: "<<connComponents<<endl;
+        cout<<"connected components: "<<connectedComponents<<endl;
         cout<<"extra edges: "<<extraEdges<<endl;
 
-        if(extraEdges >= connComponents-1) return connComponents-1;
+        if(extraEdges >= connectedComponents-1) return connectedComponents-1;
         return -1;
     }
 };
