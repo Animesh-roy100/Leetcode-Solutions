bfb4f2a0cce46edb42f3bd33206adf18eadcf6aa
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-01-15 11:31:45 +0530

    Time: 3 ms (6.84%), Space: 12.4 MB (81.65%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.cpp
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -4,19 +4,21 @@ public:
         int n=image.size();
 
         for(int i=0; i<n; i++) {
-            for(int j=0; j<n/2; j++) {
-                image[i][j] = image[i][j] ^ image[i][n-j-1];
-                image[i][n-j-1] = image[i][j] ^ image[i][n-j-1];
-                image[i][j] = image[i][j] ^ image[i][n-j-1];
+            reverse(image[i].begin(), image[i].end());
+            for(int j=0; j<n; j++) {
+                // image[i][j] = image[i][j] ^ image[i][n-j-1];
+                // image[i][n-j-1] = image[i][j] ^ image[i][n-j-1];
+                // image[i][j] = image[i][j] ^ image[i][n-j-1];
+                image[i][j] = image[i][j] ^ 1;
             }
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(image[i][j]) image[i][j] = 0;
-                else image[i][j] = 1;
-            }
-        }
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         if(image[i][j]) image[i][j] = 0;
+        //         else image[i][j] = 1;
+        //     }
+        // }
 
         return image;
     }
