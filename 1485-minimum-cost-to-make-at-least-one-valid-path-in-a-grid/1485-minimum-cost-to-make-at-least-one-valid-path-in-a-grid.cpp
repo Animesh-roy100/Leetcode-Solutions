fec0f23a401317ed32f8eb9875fcbdd0f092bb4a
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-01-21 20:37:34 +0530

    Time: 11 ms (83.34%), Space: 26.1 MB (41.84%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     typedef pair<int, pair<int, int>> piii;
+    typedef pair<int, int> pii;
+
     int minCost(vector<vector<int>>& grid) {
         int m=grid.size(), n=grid[0].size();
         vector<pair<int, int>> dir{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // right, left, down, up
 
-        priority_queue<piii, vector<piii>, greater<piii>> pq;
-        pq.push({0, {0, 0}});  // {cost, {x, y}}
+        // priority_queue<piii, vector<piii>, greater<piii>> pq;
+        // pq.push({0, {0, 0}});  // {cost, {x, y}}
+        deque<pii> dq;
+        dq.push_front({0, 0});
 
         vector<vector<int>> cost(m, vector<int> (n, INT_MAX));
         cost[0][0] = 0;
 
-        while(!pq.empty()) {
-            int currCost = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            pq.pop();
+        while(!dq.empty()) {
+            // int currCost = pq.top().first;
+            // int x = pq.top().second.first;
+            // int y = pq.top().second.second;
+            // pq.pop();
 
-            if(currCost > cost[x][y]) continue;
+            int x = dq.front().first;
+            int y = dq.front().second;
+            dq.pop_front();
+
+            // if(currCost > cost[x][y]) continue;
 
             int ind=0;
             for(auto it: dir) {
@@ -26,8 +34,7 @@ public:
                 ind++;
                 if(newX<0 or newX>=m or newY<0 or newY>=n) continue;
 
-                int newCost=currCost;
-                
+                int newCost=cost[x][y];
                 // check if direction is same as previous
                 if(grid[x][y] != ind) {
                     newCost++;
@@ -35,7 +42,13 @@ public:
 
                 if(newCost < cost[newX][newY]) {
                     cost[newX][newY] = newCost;
-                    pq.push({newCost, {newX, newY}});
+                    // pq.push({newCost, {newX, newY}});
+
+                    if(grid[x][y] == ind) {
+                        dq.push_front({newX, newY});
+                    } else {
+                        dq.push_back({newX, newY});
+                    }
                 }
             }
         }
