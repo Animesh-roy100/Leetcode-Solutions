8e4b11d0cb543430013090fe2cb265bca82bf9ef
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-01-29 13:16:06 +0530

    Time: 0 ms (100%), Space: 13.1 MB (47.29%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> parent, rank;
+
+    int findParent(int node) {
+        if(parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu != pv) {
+            if(rank[pu] > rank[pv]){
+                parent[pv] = pu;
+            } else if(rank[pu] < rank[pv]) {
+                parent[pu] = pv;
+            } else {
+                parent[pv] = pu;
+                rank[pu]++;
+            }
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        parent.resize(n+1);
+        rank.resize(n+1, 0);
+        for(int i=1; i<=n; i++) parent[i]=i;
+
+        for(auto edge: edges){
+            int u = edge[0], v=edge[1];
+            if(findParent(u) == findParent(v)) return edge;
+            unionByRank(u, v);
+        }
+
+        return {};
+    }
+};
