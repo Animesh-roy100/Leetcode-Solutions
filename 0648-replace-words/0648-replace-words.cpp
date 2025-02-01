c5658443fbbfaa89bdb99ea8afa29cb9c4a88405
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-02-01 19:28:15 +0530

    Time: 816 ms (10.78%), Space: 515.1 MB (6.16%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,39 +1,82 @@
+class TrieNode {
+public:
+    bool isComplete;
+    TrieNode* children[26];
+
+    TrieNode() {
+        isComplete = false;
+        memset(children, 0, sizeof(children));
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string str){
+        TrieNode* temp = root;
+        for(char c: str) {
+            int idx = c-'a';
+            if(temp->children[idx] == nullptr) temp->children[idx] = new TrieNode();
+            temp = temp->children[idx];
+        }
+        temp->isComplete = true;
+    }
+
+    bool searchWord(string word) {
+        TrieNode* temp = root;
+        for(char c: word){
+            int idx = c-'a';
+            if(temp->children[idx] == nullptr) return false;
+            temp = temp->children[idx];
+        }
+        return temp->isComplete;
+    }
+};
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<string, int> dict;
-        for(string str: dictionary) {
-            dict[str]++;
-        }
+        Trie t;
+        for(string str: dictionary) t.insert(str);
+
         vector<string> words;
-        string s = "";
-        for(int i=0; i<sentence.length(); i++) {
-            if(sentence[i] == ' ') {
-                words.push_back(s);
-                s = "";
+        int n=sentence.length();
+        string word="";
+        for(int r=0; r<n; r++) {
+            if(sentence[r]==' ') {
+                cout<<word<<endl;
+                words.push_back(word);
+                word="";
             } else {
-                s += sentence[i];
+                word+=sentence[r];
             }
         }
-        words.push_back(s);
-        s = "";
-        
-        for(int i=0; i<words.size(); i++)  {
-            for (int j=0; j<words[i].length(); j++) {
-                string str = words[i].substr(0, j);
-                if(dict.find(str) != dict.end()) {
+        words.push_back(word);
+
+        for(int i=0; i<words.size(); i++) {
+            string str="";
+            for(int j=0; j<words[i].length(); j++) {
+                str += words[i][j];
+                if(t.searchWord(str)) {
+                    cout<<str<<" ";
                     words[i] = str;
+                    break;
                 }
             }
         }
-        
-        string ans = "";
-        ans += words[0];
-        for (int i=1; i<words.size(); i++) {
+
+        string ans="";
+        for(string word: words){
+            ans += word;
             ans += " ";
-            ans += words[i];
         }
-        
+        ans.pop_back();
+
         return ans;
     }
 };
