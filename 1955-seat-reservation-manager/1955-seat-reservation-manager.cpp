6b75a1e4f218d77a069b4ace949f6a01e1866902
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-02-08 21:38:52 +0530

    Time: 31 ms (100%), Space: 149.6 MB (94.34%) - LeetHub

--- 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
@@ -1,26 +1,37 @@
 class SeatManager {
 public:
-    vector<int> seat;
+    // vector<int> seat;
+
     priority_queue<int, vector<int>, greater<int>> pq;
+    int seatNumber;
+    
     SeatManager(int n) {
-        seat.resize(n, 0);
-        for(int i=0; i<n; i++) pq.push(i);
+        // seat.resize(n, 0);
+        // for(int i=0; i<n; i++) pq.push(i);
+        seatNumber = 1;
     }
     
     int reserve() {
+        // if(!pq.empty()) {
+        //     int seatNum = pq.top();
+        //     pq.pop();
+        //     seat[seatNum] = 1;
+        //     return seatNum+1;
+        // }
+
         if(!pq.empty()) {
             int seatNum = pq.top();
             pq.pop();
-            seat[seatNum] = 1;
-            return seatNum+1;
+            return seatNum;
         }
-        
-        return -1;
+
+        return seatNumber++;
     }
     
     void unreserve(int seatNumber) {
-        seat[seatNumber-1] = 0;
-        pq.push(seatNumber-1);
+        // seat[seatNumber-1] = 0;
+        // pq.push(seatNumber-1);
+        pq.push(seatNumber);
     }
 };
 
