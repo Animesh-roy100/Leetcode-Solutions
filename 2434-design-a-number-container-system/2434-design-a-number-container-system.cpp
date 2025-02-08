b1751bad91f5e9f71021b76c5bd13175be7dbdfb
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-02-08 21:12:57 +0530

    Time: 2927 ms (5%), Space: 401 MB (5%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,27 +1,41 @@
 class NumberContainers {
 public:
     unordered_map<int, int> container; // {index, number}
-    unordered_map<int, set<int>> numberIdx;
+    // unordered_map<int, set<int>> numberIdx;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> numberIdx;
     NumberContainers() {
         
     }
     
     void change(int index, int number) {
-        if(container.find(index) != container.end()) {
-            int oldNum = container[index];
-            numberIdx[oldNum].erase(index);
+        // if(container.find(index) != container.end()) {
+        //     int oldNum = container[index];
+        //     numberIdx[oldNum].erase(index);
 
-            if(numberIdx[oldNum].empty()) {
-                numberIdx.erase(oldNum);
-            }
-        }
+        //     if(numberIdx[oldNum].empty()) {
+        //         numberIdx.erase(oldNum);
+        //     }
+        // }
 
         container[index] = number;
-        numberIdx[number].insert(index);
+        // numberIdx[number].insert(index);
+        numberIdx[number].push(index);
     }
     
     int find(int number) {
-        return (numberIdx.find(number) != numberIdx.end()) ? *numberIdx[number].begin() : -1;
+        // return (numberIdx.find(number) != numberIdx.end()) ? *numberIdx[number].begin() : -1;
+
+        if(numberIdx.find(number) == numberIdx.end()) return -1;
+
+        auto minHeap = numberIdx[number];
+
+        while(!minHeap.empty()) {
+            int index = minHeap.top();
+            minHeap.pop();
+            if(container[index] == number) return index;
+        }
+
+        return -1;
     }
 };
 
