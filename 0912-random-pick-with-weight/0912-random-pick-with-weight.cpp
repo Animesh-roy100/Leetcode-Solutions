8d2b972bc40fb741ae2b16218720a73c3fdc1f5d
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-02-18 18:44:50 +0530

    Time: 15 ms (65.5%), Space: 47.2 MB (23.66%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
     vector<int> sum;
+    int totalSum;
 public:
 
     Solution(vector<int>& w) {
@@ -9,24 +10,25 @@ public:
         for(int i=1; i<w.size(); i++) {
             sum.push_back(w[i] + sum.back());
         }
+        totalSum = sum.back();
     }
     
     int pickIndex() {
-        int totalSum = sum.back();
         int random = rand() % totalSum;
-        int n=sum.size();
-        int l=0, r=n-1;
-        int ans=0;
-        while(l<=r) {
-            int mid = l + (r-l)/2;
-            if(sum[mid] > random) {
-                ans = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
-            }
-        }
-        return ans;
+        return upper_bound(sum.begin(), sum.end(), random) - sum.begin();
+        // int n=sum.size();
+        // int l=0, r=n-1;
+        // int ans=0;
+        // while(l<=r) {
+        //     int mid = l + (r-l)/2;
+        //     if(sum[mid] > random) {
+        //         ans = mid;
+        //         r = mid-1;
+        //     } else {
+        //         l = mid+1;
+        //     }
+        // }
+        // return ans;
     }
 };
 
