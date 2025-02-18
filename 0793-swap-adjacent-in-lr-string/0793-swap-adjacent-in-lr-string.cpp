fb35246d64268a33ea2bad92df0b4f3a8c728296
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-02-18 15:02:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.cpp
+++ 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     bool canTransform(string start, string result) {
-        int n=start.length();
-        string s1, s2;
-        for(int i=0; i<n; i++){
-            if(start[i] != 'X') s1 += start[i];
-            if(result[i] != 'X') s2 += result[i];
+        string s1="", s2="";
+
+        for(int i=0; i<start.size(); i++) {
+            if(start[i] != 'X') s1.push_back(start[i]);
+            if(result[i] != 'X') s2.push_back(result[i]);
         }
 
         if(s1 != s2) return false;
 
         int l=0, r=0;
+        int n=start.size();
         while(l<n and r<n) {
             if(start[l] == 'X') l++;
             else if(result[r] == 'X') r++;
             else {
-                if((start[l] == 'L' and l<r) or (start[l] == 'R' and l>r)) return false;
-                l++, r++;
+                if((start[l] == 'L' and l<r) or (start[r] == 'R' and l>r)) return false;
+                else {
+                    l++, r++;
+                }
             }
         }
 
