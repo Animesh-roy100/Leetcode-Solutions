6688ea1975835cda7de2630f83226911543b6e08
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-02-26 03:26:47 +0530

    Time: 9 ms (5.5%), Space: 22.2 MB (11.69%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+        for(int i=0; i<intervals.size(); i++) {
+            vector<int> curr = intervals[i];
+
+            if(curr[1] < newInterval[0]) {
+                if(ans.empty() or ans.back()[1] < curr[0]) {
+                    cout<<"Hi"<<endl;
+                    ans.push_back(curr);
+                } else {
+                    cout<<"Hii"<<endl;
+                    ans.back()[1] = max(ans.back()[1], curr[1]);
+                }
+            } else if(curr[0] > newInterval[1]) {
+                // the new interval's range is before the other
+                ans.push_back(newInterval);
+                newInterval = curr;
+            } else {
+                // the new interval is in the range of the other interval
+                newInterval[0] = min(newInterval[0], curr[0]);
+                newInterval[1] = max(newInterval[1], curr[1]);
+            }
+        }
+        ans.push_back(newInterval);
+        
+        return ans;
+    }
+};
