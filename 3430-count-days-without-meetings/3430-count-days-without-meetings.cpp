9c43259cf50ed72946a7c348d974d656cf2be187
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-03-24 20:00:15 +0530

    Time: 71 ms (53.06%), Space: 126.8 MB (35.2%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int countDays(int days, vector<vector<int>>& meet) {
-        int n=meet.size();
-        sort(meet.begin(), meet.end());
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int n=meetings.size();
         vector<vector<int>> intervals;
 
         for(int i=0; i<n; i++) {
-            if(intervals.empty() or meet[i][0] > intervals.back()[1]) {
-                intervals.push_back({meet[i][0], meet[i][1]});
+            if(intervals.empty() or meetings[i][0] > intervals.back()[1]) {
+                intervals.push_back(meetings[i]);
             } else {
-                intervals.back()[1] = max(intervals.back()[1], meet[i][1]);
+                intervals.back()[1] = max(intervals.back()[1], meetings[i][1]);
             }
         }
 
-        for(auto it: intervals) {
-            days -= (it[1]-it[0]+1);
+        for(auto interval: intervals) {
+            days -= (interval[1] - interval[0] + 1);
         }
 
         return days;
