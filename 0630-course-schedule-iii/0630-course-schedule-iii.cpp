d6b254f3b491c83d50bc4fefd6891bb70a1e530a
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-07-03 21:13:22 +0530

    Time: 26 ms (70.79%), Space: 60.4 MB (77.43%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+        return a[1] < b[1];
+    }
+
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int currentTime=0;
+        sort(courses.begin(), courses.end(), cmp);
+        priority_queue<int> pq;
+        
+
+        for(int i=0; i<courses.size(); i++) {
+            int duration = courses[i][0];
+            int lastDay = courses[i][1];
+
+            if(currentTime + duration <= lastDay) {
+                currentTime += duration;
+                pq.push(duration);
+            }
+            else if(!pq.empty() and pq.top() > duration) {
+                currentTime = currentTime - pq.top() + duration;
+                pq.pop();
+                pq.push(duration);
+            }
+        }
+
+        return pq.size();
+    }
+};
