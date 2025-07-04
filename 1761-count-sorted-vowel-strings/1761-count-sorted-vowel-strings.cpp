e96ea544487ac0d1e6594cb188bfc870e5455641
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-07-04 14:33:21 +0530

    Time: 69 ms (9.64%), Space: 7.8 MB (84.64%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int solve(int i, int vowelIdx, int n, vector<char> &vowels) {
-        if(vowelIdx == vowels.size()) return 0;
+    int solve(int i, int vowelIdx, int n) {
+        if(vowelIdx == 5) return 0;
         if(i == n) return 1;
 
-        int take = solve(i+1, vowelIdx, n, vowels);
-        int notTake = solve(i, vowelIdx+1, n, vowels);
+        int take = solve(i+1, vowelIdx, n);
+        int notTake = solve(i, vowelIdx+1, n);
 
         return take + notTake;
     }
 
     int countVowelStrings(int n) {
-        vector<char> vowels{'a', 'e', 'i', 'o', 'u'};
-
-        return solve(0, 0, n, vowels);
+        return solve(0, 0, n);
     }
 };
