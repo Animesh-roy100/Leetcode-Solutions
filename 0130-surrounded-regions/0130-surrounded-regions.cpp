cc0a98ad475ec2586dad0bd8c458fdc30a2d3a50
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-07-07 20:14:07 +0530

    Time: 0 ms (100%), Space: 13.8 MB (91.79%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,56 +1,41 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& board, int i, int j) {
-        int m=board.size(), n=board[0].size();
-//         if(i<0 or i>=m or j<0 or j>=n or board[i][j]!='O') return;
-        
-//         board[i][j] = 'A';
-        
-//         bfs(board, i+1, j);
-//         bfs(board, i-1, j);
-//         bfs(board, i, j+1);
-//         bfs(board, i, j-1);
-        
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        
-        vector<pair<int, int>> dir{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while(!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            
-            if(x<0 or x>=m or y<0 or y>=n or board[x][y]!='O') continue;
-            
-            board[x][y] = 'A';
-            
-            for(auto &it: dir) {
-                q.push({x+it.first, y+it.second});
-            }
-        }
+    void solve(int i, int j, int m, int n, vector<vector<char>> &board) {
+        if(i<0 or i>=m or j<0 or j>=n or board[i][j] != 'O') return;
+
+        board[i][j] = 'A';
+
+        solve(i+1, j, m, n, board);
+        solve(i-1, j, m, n, board);
+        solve(i, j-1, m, n, board);
+        solve(i, j+1, m, n, board);
     }
-    
+
     void solve(vector<vector<char>>& board) {
         int m=board.size(), n=board[0].size();
-        
+
         for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {    
-                if(i==0  or j==0 or i==m-1 or j==n-1) {
-                    if(board[i][j] == 'O') {
-                        bfs(board, i, j);
-                    }
-                }
+            if(board[i][0] == 'O') {
+                solve(i, 0, m, n, board);
+            }
+            if(board[i][n-1] == 'O') {
+                solve(i, n-1, m, n, board);
             }
         }
-        
+
+        for(int j=0; j<n; j++) {
+            if(board[0][j] == 'O') {
+                solve(0, j, m, n, board);
+            }
+            if(board[m-1][j] == 'O') {
+                solve(m-1, j, m, n, board);
+            }
+        }
+
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if (board[i][j] == 'A') {
-                    board[i][j] = 'O';
-                } else if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                }
+                if(board[i][j] == 'A') board[i][j] = 'O';
+                else if(board[i][j] == 'O') board[i][j] = 'X';
             }
         }
     }
