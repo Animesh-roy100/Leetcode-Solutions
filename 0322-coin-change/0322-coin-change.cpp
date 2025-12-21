b0384996589e269dec65512f80881b56783eb0ef
Animesh Roy <58486858+Animesh-roy100@users.noreply.github.com>
2025-12-21 18:52:38 +0530

    Time: 54 ms (19.05%), Space: 54.1 MB (7.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-//     int solve(int n, int amt, vector<int> &coins, vector<vector<int>> &dp) {
-//         if(amt == 0) return 0;
-//         if(n == 0) return INT_MAX-1;
-//         if(dp[n][amt] != -1) return dp[n][amt];
-        
-//         if(coins[n-1] <= amt) {
-//             return dp[n][amt] = min(1 + solve(n, amt-coins[n-1], coins, dp), solve(n-1, amt, coins, dp));
-//         } else {
-//             return dp[n][amt] = solve(n-1, amt, coins, dp);
-//         }
-//     }
-    
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        // vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        // int ans = solve(n, amount, coins, dp);
-        // return ans == INT_MAX-1 ? -1 : ans;
-        
-        vector<int> dp(amount+1, INT_MAX-1);
-        dp[0] = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=coins[i]; j<=amount ; j++) {
-                dp[j] = min(dp[j], 1 + dp[j-coins[i]]);
-            }
+    int solve(int n, vector<int> &coins, int amount, vector<vector<int>> &dp) {
+        if(amount==0) return 0;
+        if(n == 0) return INT_MAX-1;
+        if(dp[n][amount] != -1) return dp[n][amount];
+
+        if(coins[n-1] <= amount) {
+            return dp[n][amount] = min(1 + solve(n, coins, amount-coins[n-1], dp), solve(n-1, coins, amount, dp));
+        } else {
+            return dp[n][amount] = solve(n-1, coins, amount, dp);
         }
-        
-        return dp[amount] == INT_MAX-1 ? -1 : dp[amount];
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
+        int ans = solve(n, coins, amount, dp);
+        return ans == INT_MAX-1 ? -1 : ans;
     }
 };
